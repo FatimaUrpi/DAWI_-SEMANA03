e45de0ff60e7e53eaf0c1a464c6e5aac8591d3f3
FatimaUrpi <c200509fatima.chavez09@gmail.com>
2024-04-06 12:57:20 -0500

    mensaje

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddAlumnoComponent } from './components/add-alumno/add-alumno.component';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+     {path: "addModalidad", component: AddAlumnoComponent  }
+
+
+];
